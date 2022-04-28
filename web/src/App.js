0cbc7327587968c824fd5aef11ca1a1bb88ab8d5
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-28 11:35:31 -0400

    update

--- web/src/App.js
+++ web/src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
           {/* Home View */}
         
-        <br></br>
-        <img src={logo} className="inl App-logo" alt="logo" /> Home
-      </header>
+            <br></br>
+            <img src={logo} className="inl App-logo" alt="logo" /> Test
     </div>
   );
 }
