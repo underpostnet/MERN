5dbc0c543c14faa1e57caa74caddff109cd19b10
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-28 15:42:31 -0400

    update

--- web/src/components/form-libro.js
+++ web/src/components/form-libro.js
@@ -1,10 +1,38 @@
+import { Component } from "react";
+import SimpleReactValidator from 'simple-react-validator';
 
-function FormLibro(){
-    return (
-        <div>
-            FormLibro
-        </div>
-    )
+export default class FormLibro extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {nombreLibro: ''};
+    
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+   
+      handleSubmit(event) {
+        console.log('handleSubmit', event);
+        console.log('nombreLibro', this.state.nombreLibro);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+
+
+            <input 
+                placeholder="Nombre Libro" 
+                type="text" 
+                value={this.state.nombreLibro} 
+                onChange={e => this.setState({ nombreLibro: e.target.value })} 
+            />
+
+
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
+
 }
-
-export default FormLibro;
