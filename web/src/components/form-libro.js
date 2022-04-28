a3eb1a94de7c21ad54bcc362e2f46ed9727119cc
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-28 16:03:24 -0400

    update

--- web/src/components/form-libro.js
+++ web/src/components/form-libro.js
@@ -5,7 +5,15 @@ export default class FormLibro extends Component {
     
     constructor(props) {
         super(props);
-        this.state = {nombreLibro: ''};
+
+        this.state = {
+          ISBN: "",
+          nombreLibro: "",
+          autor: "",
+          editorial: "",
+          portada: "",
+          paginas: 0        
+        };
     
         this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -13,10 +21,9 @@ export default class FormLibro extends Component {
 
       }
     
-   
       handleSubmit(event) {
         console.log('handleSubmit', event);
-        console.log('nombreLibro', this.state.nombreLibro);
+        console.log(JSON.stringify(this.state, null, 4));
         event.preventDefault();
 
         if (this.validator.allValid()) {
@@ -44,6 +51,38 @@ export default class FormLibro extends Component {
                 onChange={e => this.setState({ nombreLibro: e.target.value })} 
             />
             {this.validator.message('nombreLibro', this.state.nombreLibro, 'required')}
+
+            <input 
+                placeholder="ISBN" 
+                type="text" 
+                value={this.state.ISBN} 
+                onChange={e => this.setState({ ISBN: e.target.value })} 
+            />
+            {this.validator.message('ISBN', this.state.ISBN, 'required')}
+
+            <input 
+                placeholder="Autor" 
+                type="text" 
+                value={this.state.autor} 
+                onChange={e => this.setState({ autor: e.target.value })} 
+            />
+            {this.validator.message('autor', this.state.autor, 'required')}
+
+            <input 
+                placeholder="Editorial" 
+                type="text" 
+                value={this.state.editorial} 
+                onChange={e => this.setState({ editorial: e.target.value })} 
+            />
+            {this.validator.message('editorial', this.state.editorial, 'required')}
+
+            <input 
+                placeholder="Numero de Paginas" 
+                type="number" 
+                value={this.state.paginas} 
+                onChange={e => this.setState({ paginas: parseInt(e.target.value) })} 
+            />
+            {this.validator.message('paginas', this.state.paginas, 'required')}
 
 
             <input className="in" type="submit" value="Submit" />
