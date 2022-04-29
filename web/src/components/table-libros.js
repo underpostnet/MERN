036eefc479a36cc5d2a2015a14fe8740db22fb8f
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-29 11:56:33 -0400

    update

--- web/src/components/table-libros.js
+++ web/src/components/table-libros.js
@@ -2,6 +2,7 @@
 import { Component } from "react";
 // import SimpleReactValidator from 'simple-react-validator';
 import LibrosServices from '../services/libros/libros.service';
+import parse from 'html-react-parser';
 
 export default class FormLibro extends Component {
     
@@ -23,11 +24,35 @@ export default class FormLibro extends Component {
         });               
     }
 
+    renderTable(){
+        let _render = "<table>";   
+        
+        _render += "<tr>";
+        Object.keys(this.state.dataLibros.data[0]).map( 
+        _header => {
+            _render += "<th>" + _header + "</th>";
+        });
+        _render += "</tr>";
+
+        this.state.dataLibros.data.map( 
+        _item => {
+            _render += "<tr>";
+            Object.keys(_item).map( 
+                _key => {
+                    _render += "<th>" + _item[_key] + "</th>";
+                });
+            _render += "</tr>";
+        });
+      
+        _render += "</table>";
+        return parse(_render);
+    }
+
     render(){
         return (
-            <pre>
-                {JSON.stringify(this.state.dataLibros.data, null, 4)}
-            </pre>  
+         <div>
+             {this.state.dataLibros.data.length > 0 ? this.renderTable() : "" }
+         </div>              
          )
     }
 
